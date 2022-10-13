0432106c590eac9b1ff9a8390ab341cbcc1494ae
Dulon18 <dulon587@gmail.com>
2022-10-13 22:25:36 +0600

    Get location information from ip address in Laravel

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('get-ip-details', function () {
+	$ip = '157.180.220.120';
+    $data = \Location::get($ip);
+    dd($data);
+});
